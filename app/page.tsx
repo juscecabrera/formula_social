d64b1d378c7530ebf66b5f702d7458cdce0646af
Jusce Cabrera <jusce240@gmail.com>
2025-01-19 13:04:47 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  
+  return (
+    <div className="p-10">
+      <h1 className="font-bold">Formula Social</h1>
+    </div>
+  );
+}
